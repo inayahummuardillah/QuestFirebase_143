196efcd27729d73b4e4b0fa0ac504d69c0242a93
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2025-01-13 11:50:32 +0630

    Menambahkan fungsi untuk mereset form dan mengatur ulang state UI ke kondisi idle.

--- app/src/main/java/com/example/p9/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/p9/ui/viewmodel/InsertViewModel.kt
@@ -41,21 +41,29 @@ class InsertViewModel(
         uiEvent = uiEvent.copy(isEntryValid = errorState)
         return errorState.isValid()
     }
-
     fun insertMhs() {
-        if (validateFields()) {
-            viewModelScope.launch {
-                uiState = FormState.Loading
-                try {
-                    mhs.insertMhs(uiEvent.insertUiEvent.toMhsModel())
-                    uiState = FormState.Success("Data berhasil disimpan")
-                } catch (e: Exception) {
-                    uiState = FormState.Error("Data gagal disimpan")
-                }
+        if (validateFields()) { viewModelScope.launch {
+            uiState = FormState.Loading
+            try {
+                mhs.insertMhs(uiEvent.insertUiEvent.toMhsModel())
+                uiState = FormState.Success("Data berhasil disimpan")
+            } catch (e: Exception) {
+                uiState = FormState.Error("Data gagal disimpan")
             }
+        }
         } else {
             uiState = FormState.Error("Data tidak valid")
         }
     }
 
+    fun resetForm() {
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
+
+    fun reserSnackBarMessage() {
+        uiState = FormState.Idle
+    }
 }
+
+
