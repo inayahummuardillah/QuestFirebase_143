131eb37eabbedb4f77c20904ad01b2ecef3e955c
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2025-01-13 12:03:45 +0630

    Menambahkan fungsi PengelolaHalaman untuk mengatur navigasi antar halaman dengan menggunakan NavHost.

---
+++ app/src/main/java/com/example/p9/ui/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,42 @@
+package com.example.p9.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.p9.ui.navigasi.DestinasiHome
+import com.example.p9.ui.navigasi.DestinasiInsert
+import com.example.p9.ui.pages.InsertView
+import com.example.p9.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
