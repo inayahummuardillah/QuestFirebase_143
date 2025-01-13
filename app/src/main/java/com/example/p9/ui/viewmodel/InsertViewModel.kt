34c3e15b12fdee7db3e5006ef16a1df58205a2bd
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2025-01-13 11:47:39 +0630

    Menambahkan fungsi validasi untuk memeriksa kelengkapan dan kebenaran input pengguna pada form.

---
+++ app/src/main/java/com/example/p9/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.p9.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.p9.model.Mahasiswa
+import com.example.p9.repository.MhsRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(
+    private val mhs: MhsRepository
+) : ViewModel() {
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // Memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
+
+    // Validasi data input pengguna
+    fun validateFields(): Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else
+                "Jenis Kelamin tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else
+                "Angkatan tidak boleh kosong"
+        )
+
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+}
