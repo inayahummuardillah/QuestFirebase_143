dfc96c99222c6ae72d7ea706f7b2d4a7a9e1331b
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2025-01-13 11:51:50 +0630

    Menambahkan sealed class FormState untuk mengelola berbagai status UI (Idle, Loading, Success, Error).

--- app/src/main/java/com/example/p9/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/p9/ui/viewmodel/InsertViewModel.kt
@@ -66,4 +66,9 @@ class InsertViewModel(
     }
 }
 
-
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
