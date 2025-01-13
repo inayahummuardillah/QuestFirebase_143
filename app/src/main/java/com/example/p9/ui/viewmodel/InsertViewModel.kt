0101813fec5eaf4d1a198c4fd546706222201d92
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2025-01-13 11:57:04 +0630

    Menambahkan fungsi ekstensi untuk mengonversi MahasiswaEvent menjadi entity Mahasiswa.

--- app/src/main/java/com/example/p9/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/p9/ui/viewmodel/InsertViewModel.kt
@@ -100,4 +100,12 @@ data class MahasiswaEvent(
     val angkatan: String = ""
 )
 
-
+// Menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
